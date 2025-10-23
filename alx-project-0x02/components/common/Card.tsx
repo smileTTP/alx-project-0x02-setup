de0466776f289c5753403e40c13ac5261e829f4f
SmileTTP <ibtissamlazraf@gmail.com>
2025-10-23 01:07:47 +0100

    edit

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,1 +1,7 @@
 
+const Card: React.FC = () => {
+    return (
+        <div></div>
+    );
+}
+export default Card;
--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,1 +1,4 @@
-
+export interface CardProps {
+    title: string;
+    content: string;
+}
