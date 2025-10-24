dcd7bcfbfeae12f334751524a5440f555a0ffa3a
SmileTTP <ibtissamlazraf@gmail.com>
2025-10-24 14:42:22 +0100

    cards

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,7 +1,13 @@
+import { type CardProps } from "@/interfaces";
 
-const Card: React.FC = () => {
+const Card: React.FC<CardProps> = ({title, content}) => {
     return (
-        <div></div>
+        <div className="max-w-xl mx-auto my-6 p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+            <div className="mb-4">
+                <h2 className="text-black font-semibold">{title}</h2>
+            </div>
+            <p className="text-gray-800">{content}</p>
+        </div>
     );
 }
 export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,12 +1,26 @@
 import Header from "@/components/layout/Header";
+import Card from "@/components/common/Card";
+import { CardProps } from "@/interfaces";
+import { useState } from "react";
 
 const Home: React.FC = () => {
+    const [posts, setPosts] = useState<CardProps[]>([
+        {title: "Card no.1", content: "Those who carried the past carried the dead, and those who chased the future died of cardiac arrest."}, 
+        {title: "Card no.2", content: "What is the point of shrouding yourself in silence? The grave will do that for you for all eternity."}
+    ]);
     return (
         <div>
             <Header/>
-            <div className="flex justify-center items-center h-screen">
+            <main className="p-4">
+            <div className="flex justify-center">
                 <h1 className="text-7xl font-thin">Home</h1>
             </div>
+                <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-2">
+                {posts.map((post, index) => (
+                    <Card key={index} title={post.title} content={post.content} />
+                ))}
+                </div>
+            </main>
         </div>
     );
 }
