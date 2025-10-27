dc142ba11e43ceb2841372c27f6a7bb408610111
SmileTTP <ibtissamlazraf@gmail.com>
2025-10-27 20:27:06 +0100

    header

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -2,20 +2,21 @@ import Link from 'next/link';
 
 const Header: React.FC = () => {
     return (
-    <header className="bg-white text-black shadow-md py-4">
-        <div className="container mx-auto flex justify-between items-center px-4">
-        <nav>
-            <ul className="flex space-x-6">
-            <li className="hover:underline">
+    <header className="bg-white text-black shadow-md py-4 font-bold">
+        <div className="container mx-auto flex justify-between px-4">
+        <nav className="flex space-x-5 justify-center">
+            
+            <ul className="flex space-x-6 items-center">
+            <li className="hover:underline px-2 p-2">
                 <Link href="/about">About</Link>
             </li>
-            <li className="hover:underline">
+            <li className="hover:underline px-2 p-2">
                 <Link href="/home">Home</Link>
             </li>
-            <li className="hover:underline">
+            <li className="hover:underline px-2 p-2">
                 <Link href="/posts">Posts</Link>
             </li>
-            <li className="hover:underline">
+            <li className="hover:underline px-2 p-2">
                 <Link href="/users">Users</Link>
             </li>
             </ul>
